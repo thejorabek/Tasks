1c11ef4bf1c73c2a3da21e14490c9c9f6a955fa3
thejorabek <boyxorozovjorabek@gmail.com>
2024-11-01 21:12:58 +0500

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Jorabek Boyxorozov on 01/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tableView:
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
